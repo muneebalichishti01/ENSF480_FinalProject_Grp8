b249f03d149b2d49c57e1e19a530af720bda95ef
Sarim-Sheikh-2003 <sheikh.sarim@ucalgary.ca>
2023-11-27 15:49:16 -0700

    Wrote the code for Database.java. Still work in progress needs RegisteredUser class, FlightInfo class and the MYSQL database to complete.

--- Database.java
+++ Database.java
@@ -2,22 +2,76 @@ import java.util.*;
 import java.sql.*;
 
 public class Database {
-    private HashMap<Integer, RegisteredUser> regUsers; //hashmap holding object of class RegisteredUser
-    private ArrayList<String> flightAttendant; //flight attendant name (for login)
-    private HashMap<Integer, FlightInfo> flightInfo; //hashmap holding flight information, to gather data such as seat info, passanger list, etc.
-    private HashMap<String, Integer> seatPrice; //hashmap holding seat name and its corresponding price (ordinary, comfort, etc.)
-    final private Connection DB_CONNECT;
+  private Connection dbConnect;
+  private ResultSet results;
 
-    public Database(String dbName, String user, String password, ) throws SQLException {
-        
-    }
+  // Selects
+  private static final String queryRegisteredUser = "SELECT * FROM RegisteredUser";
+  private static final String queryFlightInfo = "SELECT * FROM FlightInfo";
+  private static final String querySeatPrice = "SELECT * FROM SeatPrice";
+  private static final String queryFlightAttendants = "SELECT * FROM FlightAttendants";
+
+  private HashMap<Integer, RegisteredUser> regUsers = new HashMap<Integer, RegisteredUser>();              //hashmap holding object of class RegisteredUser
+  private HashMap<Integer, FlightInfo> flightInfo = new HashMap<Integer, FlightInfo>();                    //hashmap holding flight information, to gather data such as seat info, passanger list, etc.
+  private HashMap<String, Integer> seatPrice = new HashMap<String, Integer>();                             //hashmap holding seat name and its corresponding price (ordinary, comfort, etc.)
+  private ArrayList<String> flightAttendants = new ArrayList<String>();                                    //flight attendant name (for login)
     
-
-    public void creatHashMaps(){
-      try{
-
-      }
+  /* Create a connection to the database */
+  public void createConnection() {
+    try {
+      dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/ewr", "oop", "password");
+    } 
+    catch (SQLException e) {
+      System.err.println("SQL Exception (File Not Found): " + e.getMessage());
+      e.printStackTrace();
     }
+  }
+
+  /* Close the connection to database */
+  public void closeConnection() {
+    try {
+      results.close();
+      dbConnect.close();
+    } 
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /* Pull the tables from the database and create hashmaps with IDs as keys and table objects for values */
+  public void createHashMaps() {
+    try {
+      Statement stmt = dbConnect.createStatement();
+      // Add treatments to hashmap with TaskID(key) and 
+      results = stmt.executeQuery(queryRegisteredUser);
+      while (results.next()) {
+        RegisteredUser regUser = new RegisteredUser();                                                   //Constructor for registered user to be filled in when making RegisteredUser class  
+        regUsers.put(, regUser);                                                                         //To be filled after making database
+      }
+
+      ResultSet results = stmt.executeQuery(queryFlightInfo);
+      while (results.next()) {
+        FlightInfo flight = new FlightInfo();                                                            //Constructor for registered user to be filled in when making RegisteredUser class 
+        flightInfo.put(, flight);                                                                        //To be filled after making database
+      }
+
+      results = stmt.executeQuery(querySeatPrice);
+      while (results.next()) {
+        seatPrice.put();                                                                                 //To be filled after making database
+      }
+
+      results = stmt.executeQuery(queryFlightAttendants);
+      while (results.next()) {
+        flightAttendants.add();                                                                          //To be filled after making database
+      }
+
+      stmt.close();
+
+    } 
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
 
 
