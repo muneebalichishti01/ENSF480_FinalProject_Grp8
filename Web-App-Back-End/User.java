a606b21bf07c0fd7ddd14ad5fe75ed0501741d59
Muneeb Ali <69442174+muneebalichishti01@users.noreply.github.com>
2023-11-28 08:29:01 -0700

    User.java implementation

--- Web-App-Back-End/User.java
+++ Web-App-Back-End/User.java
@@ -5,5 +5,65 @@ public class User {
     private String phoneNumber;
     private boolean hasCancellationInsurance;
 
-    // Constructors, getters, and setters
+    // Constructors
+    public User(int userId, String username, String email, String phoneNumber, boolean hasCancellationInsurance) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.hasCancellationInsurance = hasCancellationInsurance;
+    }
+
+    // Getters and Setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean hasCancellationInsurance() {
+        return hasCancellationInsurance;
+    }
+
+    public void setHasCancellationInsurance(boolean hasCancellationInsurance) {
+        this.hasCancellationInsurance = hasCancellationInsurance;
+    }
+
+    // toString method to display user information
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", hasCancellationInsurance=" + hasCancellationInsurance +
+                '}';
+    }
 }
