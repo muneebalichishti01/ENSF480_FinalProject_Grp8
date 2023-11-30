049c88fbe650bfd09d7b45288f763332bd7d46b8
Sarim-Sheikh-2003 <sheikh.sarim@ucalgary.ca>
2023-11-30 13:13:49 -0700

    Added seat functionality

--- flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/OrdinarySeat.java
+++ flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/OrdinarySeat.java
@@ -1,6 +1,14 @@
 package edu.ucalgary.oop.flightapp.logic;
 
 public class OrdinarySeat extends Seat {
+    // Constructors
+    public OrdinarySeat(int ID, int booked, int flightID, int type) {
+        this.seatID = ID;
+        this.booked = booked;
+        this.flightID = flightID;
+        this.type = type;
+    }
+
     public String getDescription() {
         return "Ordinary Seat";
     }
--- flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/Seat.java
+++ flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/Seat.java
@@ -1,6 +1,11 @@
 package edu.ucalgary.oop.flightapp.logic;
 
 public abstract class Seat {
+    protected int seatID;
+    protected int booked;
+    protected int type;
+    protected int flightID;
+
     public abstract String getDescription();
     public abstract double getCost();
 }
