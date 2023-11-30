cc6fc6fe905b488e0032144db5fa02e578d8a289
Muneeb Ali <69442174+muneebalichishti01@users.noreply.github.com>
2023-11-30 02:03:17 -0700

    removing CompanionTicket.java and embedding in payment.java

--- flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/Payment.java
+++ flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/Payment.java
@@ -3,43 +3,76 @@ package edu.ucalgary.oop.flightapp.logic;
 public class Payment {
     // Instance variables
     private int paymentId;
-    private User user;
+    private User user; // The user making the payment
     private double amount;
     private String paymentStatus;
 
+    // Attributes from CompanionTicket
+    private int companionTicketId;
+    private User primaryUser; // The primary user for the companion ticket
+    private User companionUser; // The companion user for the companion ticket
+
     // Constructor
-    public Payment(int paymentId, User user, double amount) {
+    public Payment(int paymentId, User user, double amount, int companionTicketId, User primaryUser, User companionUser) {
         this.paymentId = paymentId;
         this.user = user;
         this.amount = amount;
         this.paymentStatus = "Pending";
+
+        // Initializing the new attributes
+        this.companionTicketId = companionTicketId;
+        this.primaryUser = primaryUser;
+        this.companionUser = companionUser;
     }
 
     // Getters and Setters
     public int getPaymentId() {
         return paymentId;
     }
-    public User getUser() {
-        return user;
-    }
-    public double getAmount() {
-        return amount;
-    }
-    public String getPaymentStatus() {
-        return paymentStatus;
-    }
-
     public void setPaymentId(int paymentId) {
         this.paymentId = paymentId;
+    }
+
+    public User getUser() {
+        return user;
     }
     public void setUser(User user) {
         this.user = user;
     }
+
+    public double getAmount() {
+        return amount;
+    }
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
     public void setPaymentStatus(String paymentStatus) {
         this.paymentStatus = paymentStatus;
+    }
+
+    public int getCompanionTicketId() {
+        return companionTicketId;
+    }
+    public void setCompanionTicketId(int companionTicketId) {
+        this.companionTicketId = companionTicketId;
+    }
+
+    public User getPrimaryUser() {
+        return primaryUser;
+    }
+    public void setPrimaryUser(User primaryUser) {
+        this.primaryUser = primaryUser;
+    }
+
+    public User getCompanionUser() {
+        return companionUser;
+    }
+    public void setCompanionUser(User companionUser) {
+        this.companionUser = companionUser;
     }
 
     // Process payment method
@@ -54,5 +87,6 @@ public class Payment {
     public void updatePaymentDetails(double newAmount) {
         this.amount = newAmount;
     }
-}
 
+    // Additional methods...
+}
