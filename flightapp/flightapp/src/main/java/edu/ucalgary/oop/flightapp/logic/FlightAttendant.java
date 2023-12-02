65d1579cab4d6885be375dbb9a5306e7361802a0
“[Christy <[christy.guirguis@ucalgary.ca]>
2023-12-02 01:20:34 -0700

    fixed flight attendant

--- flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/FlightAttendant.java
+++ flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/FlightAttendant.java
@@ -4,7 +4,6 @@ package edu.ucalgary.oop.flightapp.logic;
 public class FlightAttendant {
     // Instance variables
     private int FlightAttendantId;
-<<<<<<< HEAD
     private String username;
     private String name;
 
@@ -12,14 +11,6 @@ public class FlightAttendant {
     public FlightAttendant(int FlightAttendantId, String username, String name) {
         this.FlightAttendantId = FlightAttendantId;
         this.username = username;
-=======
-    private String name;
-    // String role is removed
-
-    // Constructor
-    public FlightAttendant(int FlightAttendantId, String name) {
-        this.FlightAttendantId = FlightAttendantId;
->>>>>>> 5de12489e70d66c1c38a42d37bedda144fa350cc
         this.name = name;
     }
 
