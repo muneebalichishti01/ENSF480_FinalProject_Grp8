562a2921cd4f532894f90de039023d1baeac8f02
Muneeb Ali <69442174+muneebalichishti01@users.noreply.github.com>
2023-12-02 05:32:55 -0700

    added robust seat type handling"

--- flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/GUI/panels/FlightBookingPanel.java
+++ flightapp/flightapp/src/main/java/edu/ucalgary/oop/flightapp/logic/GUI/panels/FlightBookingPanel.java
@@ -12,6 +12,12 @@ public class FlightBookingPanel extends JFrame {
 
     private FlightInfo selectedFlight;
     private Seat selectedSeat;
+    private JComboBox<String> seatTypeComboBox;
+    private JPanel seatMapPanel;
+    private ButtonGroup seatGroup;
+    private static final String ORDINARY_SEAT = "Ordinary";
+    private static final String COMFORT_SEAT = "Comfort";
+    private static final String BUSINESS_SEAT = "Business";
 
     public FlightBookingPanel(FlightInfo selectedFlight) {
         this.selectedFlight = selectedFlight;
@@ -20,75 +26,107 @@ public class FlightBookingPanel extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        ArrayList<Seat> seats = Database.browseSeats(selectedFlight.getFlightId()); // Fetch seats from the database
-        JPanel seatMapPanel = new JPanel(new GridLayout(0, 4)); // Adjust based on the actual seat layout
-        ButtonGroup seatGroup = new ButtonGroup();
+        // Initialize components
+        initializeComponents();
 
-        for (Seat seat : seats) {
-            JRadioButton seatButton = new JRadioButton("Seat " + seat.getSeatID());
-            seatButton.setEnabled(!seat.isBooked()); // Disable if seat is already booked
-            seatButton.addActionListener(e -> selectedSeat = seat); // Set selected seat
-            seatGroup.add(seatButton);
-            seatMapPanel.add(seatButton);
-        }
+        // Add action listener to the combo box
+        seatTypeComboBox.addActionListener(e -> updateSeatMap());
 
-        // Check if there are no seats available and show a message
-        if (seats.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "There are no seats available for this flight.", "No Seats Available", JOptionPane.INFORMATION_MESSAGE);
-            dispose(); // Close the panel if no seats are available
-        }
-        
+        // Initial update of seat map
+        updateSeatMap();
+
+        // Layout
+        setLayout(new BorderLayout());
+        add(seatTypeComboBox, BorderLayout.NORTH);
+        add(seatMapPanel, BorderLayout.CENTER);
+        add(createControlPanel(), BorderLayout.SOUTH);
+    }
+
+    private void initializeComponents() {
+        String[] seatTypes = {ORDINARY_SEAT, COMFORT_SEAT, BUSINESS_SEAT};
+        seatTypeComboBox = new JComboBox<>(seatTypes);
+        seatMapPanel = new JPanel(new GridLayout(0, 4));
+        seatGroup = new ButtonGroup();
+    }
+
+    private JPanel createControlPanel() {
         JButton confirmBookingButton = new JButton("Confirm Booking");
-        confirmBookingButton.addActionListener(e -> {
-            if (selectedSeat == null) {
-                JOptionPane.showMessageDialog(FlightBookingPanel.this, "Please select a seat.");
-            } else {
-                confirmBooking();
-            }
-        });
+        confirmBookingButton.addActionListener(e -> confirmBooking());
 
-        // Create a back button
         JButton backButton = new JButton("Back");
         backButton.addActionListener(e -> {
-            // Close the current window
             dispose();
-            // Open the BrowseFlights panel only if seats are available
-            if (!seats.isEmpty()) {
-                BrowseFlights browseFlights = new BrowseFlights();
-                browseFlights.setVisible(true);
-            }
+            BrowseFlights browseFlights = new BrowseFlights();
+            browseFlights.setVisible(true);
         });
 
-        // Add the buttons to the bottom of the panel
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.add(confirmBookingButton);
-        bottomPanel.add(backButton);
-        add(seatMapPanel, BorderLayout.CENTER);
-        add(bottomPanel, BorderLayout.SOUTH);
+        JPanel controlPanel = new JPanel();
+        controlPanel.add(confirmBookingButton);
+        controlPanel.add(backButton);
+        return controlPanel;
     }
-    
+
+    private void updateSeatMap() {
+        seatMapPanel.removeAll();
+        seatGroup = new ButtonGroup(); // Reset the button group
+
+        String selectedSeatType = (String) seatTypeComboBox.getSelectedItem();
+        ArrayList<Seat> seats = Database.browseSeats(selectedFlight.getFlightId());
+
+        for (Seat seat : seats) {
+            if (seatTypeMatches(seat, selectedSeatType) && !seat.isBooked()) {
+                JRadioButton seatButton = new JRadioButton("Seat " + seat.getSeatID());
+                seatButton.addActionListener(e -> selectedSeat = seat); // Set selected seat
+                seatGroup.add(seatButton);
+                seatMapPanel.add(seatButton);
+            }
+        }
+
+        seatMapPanel.revalidate();
+        seatMapPanel.repaint();
+    }
+
     private void confirmBooking() {
         try {
-            if (selectedSeat != null) {
-                if (selectedSeat.isBooked()) {
-                    JOptionPane.showMessageDialog(this, "The selected seat is already booked.", "Error", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    // Mark the selected seat as booked in the database
-                    selectedSeat.setBooked(true);
-                    Database.editSeat(selectedSeat);
-    
-                    JOptionPane.showMessageDialog(this, "Booking confirmed for seat: " + selectedSeat.getSeatID());
-                    dispose(); // Close the panel
-    
-                    // Transition to PaymentInfoPanel or other appropriate action
-                    PaymentInfoPanel paymentInfoPanel = new PaymentInfoPanel(selectedFlight, selectedSeat);
-                    paymentInfoPanel.setVisible(true);
-                }
-            } else {
+            if (selectedSeat == null) {
                 JOptionPane.showMessageDialog(this, "Please select a seat.", "Error", JOptionPane.ERROR_MESSAGE);
+                return; // Exit if no seat is selected
             }
+    
+            // // Check if the selected seat is already booked (for safety, in case of concurrent bookings)
+            // if (selectedSeat.isBooked() || Database.checkSeatBookedStatus(selectedSeat)) {
+            //     JOptionPane.showMessageDialog(this, "The selected seat is already booked.", "Error", JOptionPane.ERROR_MESSAGE);
+            //     return; // Exit if the seat is already booked
+            // }
+    
+            // Proceed with booking if the seat is not booked
+            selectedSeat.setBooked(true);
+            Database.editSeat(selectedSeat);
+    
+            JOptionPane.showMessageDialog(this, "Booking confirmed for seat: " + selectedSeat.getSeatID());
+            dispose(); // Close the panel
+    
+            // Transition to PaymentInfoPanel or other appropriate action
+            PaymentInfoPanel paymentInfoPanel = new PaymentInfoPanel(selectedFlight, selectedSeat);
+            paymentInfoPanel.setVisible(true);
+    
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(this, "Error confirming booking: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
+    
+    private boolean seatTypeMatches(Seat seat, String seatType) {
+        int seatTypeId = seat.getType(); // Assuming getType() returns an int representing the seat type
+        switch (seatType) {
+            case ORDINARY_SEAT:
+                return seatTypeId == 1; // Assuming 1 represents ordinary seats
+            case COMFORT_SEAT:
+                return seatTypeId == 2; // Assuming 2 represents comfort seats
+            case BUSINESS_SEAT:
+                return seatTypeId == 3; // Assuming 3 represents business seats
+            default:
+                return false;
+        }
+    }
+    
 }
